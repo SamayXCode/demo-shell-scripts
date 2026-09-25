2e3e4d90cb8ea6e0046770989b237b252555c5d4
SamayXCode <negisamay6@gmail.com>
2026-09-25 16:13:27 +0000

    Converted into Function

--- backup.sh
+++ backup.sh
@@ -7,9 +7,16 @@ eg
 ./backup.sh <dest> <src>
 info
 
+
+fuction create_backup {
+
 dest=$1
 src=$2
 
 timestamp=$(date '+%Y-%m-%d-%H:%M')
 
 zip -r "$dest/backup-$timestamp.zip" $src
+echo "Backup completed"
+
+}
+create_backup
